b7d7a1d2d0c722d0627d2b9b611e5b78f81f3db9
kizarrd <51534324+kizarrd@users.noreply.github.com>
2021-06-13 21:03:05 +0900

    6.10

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import "./db";
+import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
@@ -17,6 +19,6 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () => console.log(`Server listening on port http://localhost${PORT} 🐌`);
+const handleListening = () => console.log(`✅ Server listening on port http://localhost${PORT} 🐌`);
 
 app.listen(PORT, handleListening);
