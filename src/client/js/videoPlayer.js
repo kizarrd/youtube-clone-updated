ec33fd20c85fbb372021091ea129abbf5026ae01
kizarrd <51534324+kizarrd@users.noreply.github.com>
2021-07-04 17:39:07 +0900

    11.11

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,9 +1,19 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
+const playBtnIcon = playBtn.querySelector("i");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
+const muteBtnIcon = muteBtn.querySelector("i");
 const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
+const fullScreenBtn = document.getElementById("fullScreen");
+const fullScreenIcon = fullScreenBtn.querySelector("i");
+const videoContainer = document.getElementById("videoContainer");
+const videoControls = document.getElementById("videoControls");
 
+let controlsTimeout = null;
+let controlsMovementTimeout = null;
 let volumeValue = 0.5;
 video.volume = volumeValue;
 
@@ -13,7 +23,7 @@ const handlePlay = (e) => {
     }else{
         video.pause();
     }
-    playBtn.innerText = video.paused ? "Play" : "Pause";
+    playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
 };
 
 const handleMute  = (e) => {
@@ -23,7 +33,9 @@ const handleMute  = (e) => {
     //     video.muted = true;
     // }
     video.muted = video.muted ? false : true;
-    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+    muteBtnIcon.classList = video.muted
+    ? "fas fa-volume-mute"
+    : "fas fa-volume-up";
     volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
@@ -39,7 +51,67 @@ const handleVolumeChange = (event) => {
     video.volume = volumeValue;
 };
 
+const formatTime = (seconds) => new Date((seconds)*1000).toISOString().substr(14, 5);
+
+const handleLoadedMetadata = () => {
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+    currentTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
+};
+
+const handleTimelineChange = (event) => {
+    const {
+        target: { value },
+    } = event;
+    video.currentTime = value;
+};
+
+const handleFullScreenClick = () => {
+    const fullscreen = document.fullscreenElement;
+    if(fullscreen){
+        document.exitFullscreen();
+        fullScreenIcon.classList = "fas fa-expand";
+    }else{
+        videoContainer.requestFullscreen();
+        fullScreenIcon.classList = "fas fa-compress";
+    }
+};
+
+        
+const removeShowingClass = () => videoControls.classList.remove("showing");
+
+const handleMouseMove = () => {
+    if(controlsTimeout){
+        clearTimeout(controlsTimeout);
+        controlsTimeout = null;
+    }
+    if(controlsMovementTimeout){
+        clearTimeout(controlsMovementTimeout);
+        controlsMovementTimeout = null;
+    }
+    videoControls.classList.add("showing");
+    controlsMovementTimeout = setTimeout(removeShowingClass, 1000);
+    // --> video화면 위에서 마우스 포인터가 움직일때마다 항상 controlsMovementTimeout가 실행됨. controlsMovementTimeout는 1초후에 remove showing class하는 놈. 그러므로 움직임을 멈춘다면 곧 컨트롤러가 사라지게 되는 것. 움직임을 계속 가져간다면 바로 위 if 안에서clearTimeout(controlsMovementTimeout)이 실행되기 때문에 컨트롤러가 사라지지 않음. ( set timeout이 취소됨. )
+};
+
+const handleMouseLeave = () => {
+    controlsTimeout = setTimeout(removeShowingClass, 1000);
+};
+
 playBtn.addEventListener("click", handlePlay);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadeddata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
+videoContainer.addEventListener("mousemove", handleMouseMove);
+videoContainer.addEventListener("mouseleave", handleMouseLeave);
+timeline.addEventListener("input", handleTimelineChange);
+fullScreenBtn.addEventListener("click", handleFullScreenClick);
+video.addEventListener("mousemove", handleMouseMove);
+video.addEventListener("mouseleave", handleMouseLeave);
+
 
