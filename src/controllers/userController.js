511faa6153880527bc65dba0d726802bd9bf4753
kizarrd <51534324+kizarrd@users.noreply.github.com>
2021-07-09 00:38:47 +0900

    s3 upload

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -145,7 +145,7 @@ export const postEdit = async (req, res) => {
     console.log(file);
     const updatedUser = await User.findByIdAndUpdate( 
         _id, {
-            avatarUrl: file ? file.path : avatarUrl,
+            avatarUrl: file ? file.location : avatarUrl,
             name, email, username, location,
         },
         { new: true }
